96dde8b78abb97a083142e3ec59d122eeace9c2c
Adam Bender <abender@greenenergycorp.com>
2012-05-17 18:55:15 -0600

    added final tests

--- src/test/js/looping-spec.js
+++ src/test/js/looping-spec.js
@@ -24,7 +24,19 @@ describe("Looping", function() {
 		
 		expect( keysInDict ).toEqual("a, b, c");
 		expect( valuesInDict ).toEqual("apple, banana, cherry");
-	});		
+	});
+	
+	it("should loop through contents of an array using do/while", function(){
+		var numbers = ["1","2","3","4","5","6","7","8","9","10"],
+		numberString = "",
+		i = __;
+		
+		do {
+			
+		} while (__);
+		
+		expect( numberString ).toEqual("12345678910");
+	});				
 
 });
 
--- src/test/js/namespaces-spec.js
+++ src/test/js/namespaces-spec.js
@@ -1,7 +1,38 @@
 describe("Namespaces and Modules", function() {
 
 	var __ = null;
+	
+	// simply adds 2 to the argument
+	function add2(x) {
+		return x + 2;
+	}
+	
+	var myNamespace = function() {
 
+		return {
+			// multiplies as in "add two of your argument together"
+			add2 : function(x) {
+				return x * 2; 
+			}
+		};
+
+	}();
+
+	it("should use the global function", function(){		
+		var aNumber = 6,
+		result; 
+		
+		result = __;
+		
+		expect( result ).toEqual(8);
+	});
+	
+	it("should use the namespaced function", function(){
+		var aNumber = 6,
+		result; 
+		
+		result = __;
+		
+		expect( result ).toEqual(8);
+	});
 });
-
-
